effd81c4084f7a121dd5a6dde96571ce87ffb5ea
Devang Mehra <MY_NAME@example.com>
2024-04-28 13:02:52 +0530

    ui: sign in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,18 @@
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex flex-col items-center'>
+      <h1 className='text-[30px] font-medium'>Sign In </h1>
+      <div className="p-4 flex flex-col gap-4">
+         <Input placeholder='Enter your Email'/>
+         <Input placeholder='Enter your Password'/>
+      </div>
+
+    <p className="text-[#ff0000f3]">Error</p>
+
+    </div>
   )
 }
 
